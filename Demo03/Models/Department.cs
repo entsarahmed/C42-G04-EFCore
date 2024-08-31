527929d113f9f679f555e9d367b2ba3b4219fd8e
entsarahmed <entesara500@gmail.com>
2024-08-31 19:01:21 +0300

    Lazy Loading

--- Demo03/Models/Department.cs
+++ Demo03/Models/Department.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+//using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+//[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
 namespace Demo.Models
 {
+
     internal class Department
     {
        public int Id { get; set; }
--- Demo03/Program.cs
+++ Demo03/Program.cs
@@ -133,6 +133,50 @@ namespace Demo03
             #endregion
 
 
+
+            #region 3. Lazy Loading
+
+            //Decrease Count of Request on Server
+            //Implicitly Loading
+            #region Example 01
+
+            var employee = (from E in dbContext.Employees
+                            where E.Id ==1
+                            select E).FirstOrDefault();
+
+            if (employee is not null)
+            {
+                //    // Get Data in Request and Related Data in Another Request
+
+
+
+
+                Console.WriteLine($"Employee: Name = {employee.Name}, Department ={employee.Department?.Name ?? "NA"}");
+            }
+
+            #endregion
+
+
+            #region Example 02
+            //var department =(from D in dbContext.Departments.Include(D => D.Employees)
+            //                 where D.Id == 10
+            //                 select D).FirstOrDefault();
+            //if (department != null)
+            //{
+
+
+            //    Console.WriteLine($"Department: Id = {department.Id}, Name = {department.Name}");
+
+
+            //    foreach(var employee in department.Employees)
+            //        Console.WriteLine($"......Employee: Id = {employee.Id}, Name = {employee.Name}");
+            //} 
+            #endregion
+
+            #endregion
+
+
+
         }
     }
 }
