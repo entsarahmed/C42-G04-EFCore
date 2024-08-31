faacd6925704e2a7fc41e799b461cccc9d133ca4
entsarahmed <entesara500@gmail.com>
2024-08-31 16:35:36 +0300

    Navigational Properties

--- Demo03/Data/CompanyDbcontextSeed.cs
+++ Demo03/Data/CompanyDbcontextSeed.cs
@@ -11,43 +11,43 @@ namespace Demo03.Data
 {
     internal class CompanyDbcontextSeed
     {
-        public static void Seed(ApplicationDbContext dbContext)
-        {
-            if (dbContext.Departments.Any())
-            {
-                var departmentsData = File.ReadAllText("./Demo/Data/DataSeed/departments.json");
-                var departments = JsonSerializer.Deserialize<List<Department>>(departmentsData);
+    //    public static void Seed(ApplicationDbContext dbContext)
+    //    {
+    //        if (dbContext.Departments.Any())
+    //        {
+    //            var departmentsData = File.ReadAllText("./Demo/Data/DataSeed/departments.json");
+    //            var departments = JsonSerializer.Deserialize<List<Department>>(departmentsData);
 
 
-                if (departments?.Count > 0)
-                {
-                    foreach (var department in departments)
-                    {
-                        dbContext.Departments.Add(department);
-                    }
-                    dbContext.SaveChanges();
+    //            if (departments?.Count > 0)
+    //            {
+    //                foreach (var department in departments)
+    //                {
+    //                    dbContext.Departments.Add(department);
+    //                }
+    //                dbContext.SaveChanges();
 
-                }
-            }
+    //            }
+    //        }
 
-            if (dbContext.Employees.Any())
-            {
-                var employeesData = File.ReadAllText("./Demo/Data/DataSeed/employees.json");
-                var employees = JsonSerializer.Deserialize<List<Employee>>(employeesData);
-
-
-                if (employees?.Count > 0)
-                {
-                    foreach (var employee in employees)
-                    {
-                        dbContext.Employees.Add(employee);
-                    }
-                    dbContext.SaveChanges();
-
-                }
-            }
+    //        if (dbContext.Employees.Any())
+    //        {
+    //            var employeesData = File.ReadAllText("./Demo/Data/DataSeed/employees.json");
+    //            var employees = JsonSerializer.Deserialize<List<Employee>>(employeesData);
 
 
-        }
+    //            if (employees?.Count > 0)
+    //            {
+    //                foreach (var employee in employees)
+    //                {
+    //                    dbContext.Employees.Add(employee);
+    //                }
+    //                dbContext.SaveChanges();
+
+    //            }
+   //        }
+
+
+     //   }
     }
 }
--- Demo03/Program.cs
+++ Demo03/Program.cs
@@ -13,6 +13,43 @@ namespace Demo03
 
             //CompanyDbcontextSeed.Seed(dbContext);
 
+            #region Loading Navigational Properties
+            #region Example 01
+
+            //var employee =(from E in dbContext.Employees
+            //              where E.Id ==1
+            //             select E).FirstOrDefault();
+
+            //if (employee is not null)
+            //{
+            //    // Get Data in Request and Related Data in Another Request
+
+
+
+
+            //    Console.WriteLine($"Employee: Name = {employee.Name}, Department ={employee.Department?.Name ?? "NA"}");
+            //}
+
+            #endregion
+
+
+            #region Example 02
+            //var department =(from D in dbContext.Departments
+            //                 where D.Id == 10
+            //                 select D).FirstOrDefault();
+            //if (department != null)
+            //{
+
+
+            //    Console.WriteLine($"Department: Id = {department.Id}, Name = {department.Name}");
+
+
+            //    foreach(var employee in department.Employees)
+            //        Console.WriteLine($"......Employee: Id = {employee.Id}, Name = {employee.Name}");
+            //} 
+            #endregion
+
+            #endregion
 
 
         }
