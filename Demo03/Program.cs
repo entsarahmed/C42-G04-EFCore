9a8983889790b1ba71d13debe5984565b2686856
entsarahmed <entesara500@gmail.com>
2024-08-31 16:44:09 +0300

    Explicit Loading

--- Demo03/Program.cs
+++ Demo03/Program.cs
@@ -52,6 +52,47 @@ namespace Demo03
             #endregion
 
 
+            #region 1. Explicit Loading
+            #region Example 01
+
+            //var employee =(from E in dbContext.Employees
+            //              where E.Id ==1
+            //             select E).FirstOrDefault();
+
+            //if (employee is not null)
+            //{
+            //    // Get Data in Request and Related Data in Another Request
+
+            //    dbContext.Entry(employee).Reference(E => E.Department).Load(); // Loading Explicitly
+
+
+
+            //    Console.WriteLine($"Employee: Name = {employee.Name}, Department ={employee.Department?.Name ?? "NA"}");
+            //}
+
+            #endregion
+
+
+
+            #region Example 02
+            //var department =(from D in dbContext.Departments
+            //                 where D.Id == 10
+            //                 select D).FirstOrDefault();
+            //if (department != null)
+            //{
+
+
+            //    Console.WriteLine($"Department: Id = {department.Id}, Name = {department.Name}");
+
+
+            //    dbContext.Entry(department).Collection(D => D.Employees).Load();//Explicit Loading
+            //    foreach(var employee in department.Employees)
+            //        Console.WriteLine($"......Employee: Id = {employee.Id}, Name = {employee.Name}");
+            //} 
+            #endregion
+            #endregion
+
+
         }
     }
 }
